d88203e3ddd997a89dcb4720b9d8e85d073602d8
alireza <alireza@alireza.com>
2022-01-28 12:38:06 +0330

    add items

---
+++ Mosaic_Image_Generator/CMakeFiles/MosaicIt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MosaicIt.dir/main.cpp.o"
+  "MosaicIt"
+  "MosaicIt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MosaicIt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
